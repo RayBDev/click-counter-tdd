7c28a37c0dbf6d662ace993bd852bc64cbd02bdd
RayB <raybernard1337@gmail.com>
2019-09-10 08:30:28 -0600

    Added decrement counter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    counter: 0
+    counter: 0,
+    error: false
   };
+
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1, error: false });
+  };
+
+  decrementCounter = () => {
+    if (this.state.counter > 0) {
+      this.setState({ counter: this.state.counter - 1, error: false });
+    } else {
+      this.setState({ error: true });
+    }
+  };
+
   render() {
+    let errorMessage = null;
+    if (this.state.error) {
+      errorMessage = (
+        <div data-test="error-message">Can't decrement below zero</div>
+      );
+    }
     return (
       <div data-test="component-app">
         <h1 data-test="counter-display">
@@ -13,10 +33,17 @@ class App extends Component {
         </h1>
         <button
           data-test="increment-button"
-          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+          onClick={() => this.incrementCounter()}
         >
           Increment counter
         </button>
+        <button
+          data-test="decrement-button"
+          onClick={() => this.decrementCounter()}
+        >
+          Decrement counter
+        </button>
+        {errorMessage}
       </div>
     );
   }
